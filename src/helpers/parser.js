6f9f3d10834a9ed203edee80b3cf0c2ef8b6e5a8
Roman Nefedov <nefedovroma@gmail.com>
2023-05-10 22:58:26 +0300

    [fix]: parser function
    - parse form content in JSON format

--- src/helpers/parser.js
+++ src/helpers/parser.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line no-new-func
-export const addContentItem = string => new Function(`{return (${string})}`);
+export const addContentItem = (content) => JSON.parse(content);
